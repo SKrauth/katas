14e0efb5af47e55e793af50c372c0b347158dced
SKrauth <spkrauth@gmail.com>
2021-04-21 12:35:24 -0700

    express server with mocha

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Arithmatic', function() {
+  describe('two plus two', function() {
+    it('should equal four', function() {
+      assert.equal(2 + 2, 4);
+    });
+  });
+});
